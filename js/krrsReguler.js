ce8bac60d09fb5e4f73ce323b26dfd3ec9b5cd28
Dennis Sebastian <sienshens1st@gmail.com>
2020-10-10 16:03:45 +0700

    simple change

---
+++ js/krrsReguler.js
@@ -0,0 +1,15 @@
+const userInfo = document.querySelector('.nam')
+const userNim = document.querySelector('.nim');
+const localName = localStorage.getItem('userName');
+const localNim = localStorage.getItem('userNim');
+
+const div = document.createElement('div');
+const nama = document.createElement('h4'); 
+const nim = document.createElement('h4');
+
+var textNama = document.createElement('h4');
+var textNim = document.createElement('h4');
+textNama.innerHTML = "Nama  \xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0: " + localName;
+textNim.innerHTML = "Nim \xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0: "  + localNim;
+userInfo.appendChild(textNama);
+userNim.appendChild(textNim);
