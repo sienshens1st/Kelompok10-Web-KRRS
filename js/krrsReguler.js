b191f76215049ea03b4bcfe17bfbf7c4d1d03ccc
Dennis Sebastian <sienshens1st@gmail.com>
2020-10-15 20:53:44 +0700

    add pop up after submit

--- js/krrsReguler.js
+++ js/krrsReguler.js
@@ -13,3 +13,49 @@ textNama.innerHTML = "Nama  \xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0: " 
 textNim.innerHTML = "Nim \xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0: "  + localNim;
 userInfo.appendChild(textNama);
 userNim.appendChild(textNim);
+
+
+//overlay submit
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+//overlay submit end
+
+const submit = document.getElementById("submit-ya");
+submit.addEventListener("click",()=>{
+alert("KRRS Online Submitted!");
+
+});
